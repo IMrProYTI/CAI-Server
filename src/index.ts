365fccb00a9ba8f14520233ef4463deafa7f9519
Alexander <s555b@mail.ru>
2024-03-11 19:27:42 +0300

    host everywhere

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const PORT = Number(process.env.PORT) || 8080;
 
 (() => {
 	try {
-		fastify.listen({ port: PORT });
+		fastify.listen({ port: PORT, host: "0.0.0.0" });
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
